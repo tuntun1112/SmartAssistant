2980c31b7d32e483a604a85d391a976ad68240eb
豚豚 <81475239+tuntun1112@users.noreply.github.com>
2025-06-18 11:38:38 +0800

    新增 Wi-Fi 連接功能

--- main/main.c
+++ main/main.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+// for wifi
+#include "wifi_connect.h"
 
-void app_main(void)
-{
-
+void app_main(void) {
+    //=====================================(WIFI)
+    wifi_connect("ESPTEST", "12345678"); // 連線 WiFi (SSID/PASSWORD)
+    //=====================================
 }
