e9e9ac9b6993a7b2ec07ef9b9a010b6759ff2094
豚豚 <81475239+tuntun1112@users.noreply.github.com>
2025-07-12 22:10:20 +0800

    重新開始設計

--- main/main.c
+++ main/main.c
@@ -1,8 +1,2 @@
-// for wifi
-#include "wifi_connect.h"
-
 void app_main(void) {
-    //=====================================(WIFI)
-    wifi_connect("ESPTEST", "12345678"); // 連線 WiFi (SSID/PASSWORD)
-    //=====================================
 }
